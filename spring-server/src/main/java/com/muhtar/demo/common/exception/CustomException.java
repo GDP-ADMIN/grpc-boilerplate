44f8d3f9d885a58c892eba90a587218d61abbf86
muhtarh <muhtar.hartopo@gdplabs.id>
2019-10-24 17:48:26 +0700

    move project

--- spring-server/src/main/java/com/muhtar/demo/common/exception/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.muhtar.demo.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public abstract class CustomException extends Exception {
-    public abstract HttpStatus getStatusCode();
-    public abstract String getErrorCode();
-    public abstract String getErrorMessage();
-}
--- spring-server/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO BOOKS (title, author) VALUES
-  ('clean code', 'robert c martin'),
-  ('halo', 'bandung'),
-  ('Captain America', 'Stan Lee');
