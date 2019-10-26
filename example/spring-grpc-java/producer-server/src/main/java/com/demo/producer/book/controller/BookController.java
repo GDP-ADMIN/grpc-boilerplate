b84cbe08612e3a98295984101fa36b46b5e56e9a
muhtarh <muhtar.hartopo@gdplabs.id>
2019-10-26 17:25:57 +0700

    minor fix + init new project

--- example/spring-grpc-java/producer-server/src/main/java/com/demo/producer/book/controller/BookController.java
+++ example/spring-grpc-java/producer-server/src/main/java/com/demo/producer/book/controller/BookController.java
@@ -3,8 +3,10 @@ package com.demo.producer.book.controller;
 import com.demo.producer.book.entity.Book;
 import com.demo.producer.book.service.BookCrudService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
