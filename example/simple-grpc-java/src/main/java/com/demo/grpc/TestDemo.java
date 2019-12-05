79fc412c9058e3698c562f20c7e5e7278443e6c0
muhtarh <muhtar.hartopo@gdplabs.id>
2019-12-05 09:45:52 +0700

    add interceptor example

--- example/simple-grpc-java/src/main/java/com/demo/grpc/TestDemo.java
+++ example/simple-grpc-java/src/main/java/com/demo/grpc/TestDemo.java
@@ -6,6 +6,7 @@ import com.muhtar.rpc.book.BookServiceGrpc;
 import com.muhtar.rpc.book.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 public class TestDemo {
 
@@ -16,11 +17,12 @@ public class TestDemo {
         ManagedChannel managedChannel = ManagedChannelBuilder.forAddress(HOST, PORT).usePlaintext().build();
         BookServiceGrpc.BookServiceBlockingStub bookServiceBlockingStub =
             BookServiceGrpc.newBlockingStub(managedChannel);
-
-        BookMessageList bookMessageList = bookServiceBlockingStub.findAll(Empty.newBuilder().build());
-
-        BookMessage bookMessage = bookMessageList.getBook(0);
-
-        System.out.println(bookMessage);
+        try {
+            BookMessageList bookMessageList = bookServiceBlockingStub.findAll(Empty.newBuilder().build());
+            BookMessage bookMessage = bookMessageList.getBook(0);
+            System.out.println(bookMessage);
+        } catch (StatusRuntimeException e) {
+            System.out.println("error: " + e.toString());
+        }
     }
 }
--- example/simple-grpc-java/src/main/java/com/demo/grpc/interceptor/MyInterceptor.java
+++ example/simple-grpc-java/src/main/java/com/demo/grpc/interceptor/MyInterceptor.java
@@ -1,18 +1,23 @@
 package com.demo.grpc.interceptor;
 
-import io.grpc.Metadata;
-import io.grpc.ServerCall;
-import io.grpc.ServerCallHandler;
-import io.grpc.ServerInterceptor;
+import io.grpc.*;
+
+import java.util.logging.Logger;
 
 public class MyInterceptor implements ServerInterceptor {
+
+    private static final Logger LOGGER = Logger.getLogger(MyInterceptor.class.getName());
+
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
             ServerCall<ReqT, RespT> call,
             Metadata headers,
             ServerCallHandler<ReqT, RespT> next
     ) {
-        // logic goes here
-        return null;
+        LOGGER.info("my interceptor here");
+        return next.startCall(
+            new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {},
+            headers
+        );
     }
 }
