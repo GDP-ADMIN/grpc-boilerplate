7f8f414f53ca52cfd0a9a29351f307a91b200e04
Muhtar H <34231047+muhtarh@users.noreply.github.com>
2019-12-06 11:31:22 +0700

    Update examples (#1)
    * add db
    
    * fix build.gradle
    
    * fix build.gradle
    
    * fix generate.sh script
    
    * tidy up go example
    
    * fix struct

--- example/go-grpc/generate.sh
+++ example/go-grpc/generate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-protoc -I ../../proto ../../proto/Book.proto --go_out=plugins=grpc:$GOPATH/src/gogrpc/book
+TARGET_DIR=$GOPATH/src/gogrpc/book
+if [ ! -d $TARGET_DIR ]
+then
+    echo "Create ${TARGET_DIR} directory"
+    mkdir -p $TARGET_DIR
+fi
+
+echo "Generating stub at ${TARGET_DIR} ..."
+
+protoc -I ../../proto ../../proto/Book.proto --go_out=plugins=grpc:$TARGET_DIR
